6e248eed05adb962b7bff45ec6e1d3ce08767484
gregnetols <gregnetols@gmail.com>
2017-08-31 23:19:56 -0500

    Im not a knuklehead fixes
    streamlined code, changed boarder rules

--- GameOfLife.py
+++ GameOfLife.py
@@ -46,48 +46,40 @@ def cell_birth(board, X, Y):
 #Determines if a cell is in the non displayed boarder
 #Inputs:
 #   board - Game of Life board 
-#   length - length of the board
-#   width - width of the board
-def in_boarder(board, boarderWidth, X, Y):
-    inBoarder = 0
-    if X < boarderWidth:
-        inBoarder = 1
-    if Y < boarderWidth:
-        inBoarder = 1
-    if X >= len(board[:,0]) - boarderWidth:
-        inBoarder = 1
-    if Y >= len(board[0,:]) - boarderWidth:
-        inBoarder = 1
-    return inBoarder
+#   x - x position
+#   y - y position
+def on_boarder(board, x, y):
+    if (x-1<=0) or (y-1<=0) or (x+1>=board.shape[0]) or (y+1>=board.shape[1]):
+        return 1
 
 #Determines what happens between each generation for cells in the boarder
 #Inputs
 #   board - Game of Life Board
-#   X - X Position
-#   Y -Y Position
-def boarder_rules(board, X, Y):
+#   x - x position
+#   y - y position
+def boarder_rules(board, x, y):
     return 0
 
 
 #Initailize a Game of Life Board
 #Inputs:
 #   length - length of the board
-#   width - width of the board
-#   pLive - porbablity that a cell starts alive
-def initialize_board(length, width, pLive):
+#   height - height of the board
+#   pLive - probability that a cell starts alive
+def initialize_board(length, height, pLive):
     pDead = 1 - pLive
-    board = np.random.choice([0,1], size=(length, width), p=[pDead, pLive])
+    board = np.random.choice([0,1], size=(length, height), p=[pDead, pLive])
     return board
 
 #performes a turn of the game of life
 #Inputs
 #   board - a game of life board
 #   boarderWidth - width of the boarder used to determine when to use boarder logic
-def game_of_life_turn(board, boarderWidth):
+def game_of_life_turn(board):
     newBoard = np.zeros((board.shape))
     for row in range(0, len(board[:, 0])):
         for col in range(0, len(board[0, :])):
-            if in_boarder(board, boarderWidth, row, col):
+            if on_boarder(board, row, col):
                 newBoard[row, col] = boarder_rules(board, row, col)
             elif board[row, col] == 1:
                 newBoard[row, col] = cell_survival(board, row, col)
@@ -102,12 +94,15 @@ def game_of_life_turn(board, boarderWidth):
 #   displayLength - displayed length of the board
 #   diaplyWidth - dispalyed width of the board
 #   totalGenerations - the total amount of board geneartions to iterate through
-gameInputs = {'length': 125,
-              'width': 125,
-              'boarderWidth': 3,
+gameInputs = {'height': 120,
+              'length': 120,
+              'width': 120,
+              'boarderWidth': 10,
+              'displayHeight': 100,
+              'displayWidth':100,
               'totalGenerations':200,
               'startingLive': .5,
-              'FPS': 5,
+              'FPS': 100,
               'black': (0,0,0),
               'red': (255,0,0),
               'grey': (30,30,30),
@@ -118,13 +113,13 @@ pygame.init()
 clock = pygame.time.Clock()
 
 #setup window
-window = pygame.display.set_mode((((gameInputs['length'] - 2*gameInputs['boarderWidth']) * gameInputs['cellSize']),
-                                 ((gameInputs['width'] - 2*gameInputs['boarderWidth']) * gameInputs['cellSize'])))
+window = pygame.display.set_mode(((gameInputs['displayWidth'] * gameInputs['cellSize']),
+                                 (gameInputs['displayHeight'] * gameInputs['cellSize'])))
 pygame.display.set_caption('Conway''s Game of Life')
 window.fill(gameInputs['black'])
 
 #initialize game of life board
-board = initialize_board(gameInputs['length'], gameInputs['width'], gameInputs['startingLive'])
+board = initialize_board(gameInputs['width'], gameInputs['height'],  gameInputs['startingLive'])
 
 #Main Loop
 while True:
@@ -136,11 +131,11 @@ while True:
             sys.exit()
 
     #update board
-    board = game_of_life_turn(board, gameInputs['boarderWidth'])
+    board = game_of_life_turn(board)
 
     #draw grid
-    for x in range(0, ((gameInputs['length'] - gameInputs['boarderWidth']) * gameInputs['cellSize']), gameInputs['cellSize']):
-        for y in range(0, ((gameInputs['width'] - gameInputs['boarderWidth']) * gameInputs['cellSize']), gameInputs['cellSize']):
+    for x in range(0, (gameInputs['displayWidth'] * gameInputs['cellSize']), gameInputs['cellSize']):
+        for y in range(0, (gameInputs['displayHeight'] * gameInputs['cellSize']), gameInputs['cellSize']):
             if board[x/gameInputs['cellSize']+gameInputs['boarderWidth']][y/gameInputs['cellSize']+gameInputs['boarderWidth']] == 1:
                 pygame.draw.rect(window, gameInputs['red'], [x, y, gameInputs['cellSize'], gameInputs['cellSize']])
             else:
